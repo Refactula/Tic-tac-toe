24e719481a34b984dfa0e48fced943a1b9bf5aa8
Refactula <refactula@gmail.com>
2015-12-20 14:41:37 +0200

    Add winner line game object with a script

---
+++ Assets/Scripts/WinnerLine.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinnerLine : MonoBehaviour {
+
+    public Sprite HorizontalLineSprite;
+    public Sprite VertinalLineSprite;
+    public Sprite DiagonalLineSprite;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
