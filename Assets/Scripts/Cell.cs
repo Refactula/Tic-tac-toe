07b60b6f0e3c42b51bba3e63fb2cd6375e645a3f
Refactula <refactula@gmail.com>
2015-12-22 23:30:48 +0200

    Adjust all sprites

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cell : MonoBehaviour {
+
+    public Sprite CrossSprite;
+    public Sprite NoughtSprite;
+
+}
---
+++ Assets/Scripts/Line.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Line : MonoBehaviour {
+
+    public GameObject Cell0;
+    public GameObject Cell1;
+    public GameObject Cell2;
+
+}
