79802ca3967fb28f5d095107c772957ec93cad5f
Refactula <refactula@gmail.com>
2015-12-29 23:43:39 +0200

    Add marks on the game scene

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class GameController : MonoBehaviour, IGameController {
+
+    private Game game = new Game();
+
+    public IGame Game
+    {
+        get
+        {
+            return game;
+        }
+    }
+
+    public void RequestPut(int column, int row, Mark mark)
+    {
+        game.Put(column, row, mark);
+    }
+
+
+}
---
+++ Assets/Scripts/MarkView.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MarkView : MonoBehaviour {
+
+    public Sprite CrossSprite;
+    public Sprite NoughtSprite;
+
+    public int Column;
+    public int Row;
+
+    private GameController gameController;
+
+    void Awake()
+    {
+        gameController = GetComponentInParent<GameController>();
+    }
+
+}
