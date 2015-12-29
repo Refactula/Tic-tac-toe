0f45354be5e9da27187ebccfcbcfe7492d3a57aa
Refactula <refactula@gmail.com>
2015-12-30 00:25:43 +0200

    Make it work! Without the winning line though.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,8 +1,11 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
 using System;
 
-public class GameController : MonoBehaviour, IGameController {
+public class GameController : MonoBehaviour, IGameController, IGameListener {
+
+    private const Mark HumanPlayerMark = Mark.Cross;
 
     private Game game = new Game();
 
@@ -14,10 +17,56 @@ public class GameController : MonoBehaviour, IGameController {
         }
     }
 
+    void Awake()
+    {
+        game.Subscribe(new ComputerPlayer(this, Mark.Nought, new System.Random(), ComputerPlayer.HARDNESS_HARD));
+        game.Subscribe(this);
+    }
+
+    // Request from human player.
+    public void OnClicked(int column, int row)
+    {
+        game.Put(column, row, HumanPlayerMark);
+    }
+
+    // Request from computer player.
     public void RequestPut(int column, int row, Mark mark)
     {
+        StartCoroutine("PutLater", new object[3] { column, row, mark });
+    }
+
+    public void RegisterMarkView(MarkView markView)
+    {
+        game.Subscribe(markView);
+    }
+
+    public void OnPutMark(IGame game, int column, int row, Mark mark)
+    {
+
+    }
+
+    public void OnGameOver(IGame game, Line winnerLine, Mark mark)
+    {
+        StartCoroutine("RestartLater");
+    }
+
+    public void OnNextPlayerTurn(IGame game, Mark mark)
+    {
+
+    }
+
+    IEnumerator PutLater(object[] parms)
+    {
+        yield return new WaitForSeconds(UnityEngine.Random.Range(0.1F, 0.7F));
+        int column = (int) parms[0];
+        int row = (int) parms[1];
+        Mark mark = (Mark) parms[2];
         game.Put(column, row, mark);
     }
 
-
+    IEnumerator RestartLater()
+    {
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene("GameScene");
+    }
 }
--- Assets/Scripts/MarkView.cs
+++ Assets/Scripts/MarkView.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
-public class MarkView : MonoBehaviour {
+public class MarkView : MonoBehaviour, IGameListener {
 
     public Sprite CrossSprite;
     public Sprite NoughtSprite;
@@ -16,4 +17,40 @@ public class MarkView : MonoBehaviour {
         gameController = GetComponentInParent<GameController>();
     }
 
+    void Start()
+    {
+        gameController.RegisterMarkView(this);
+    }
+
+    void OnMouseDown()
+    {
+        gameController.OnClicked(Column, Row);
+    }
+
+    public void OnPutMark(IGame game, int column, int row, Mark mark)
+    {
+        if (column == Column && row == Row)
+        {
+            var renderer = GetComponent<SpriteRenderer>();
+            renderer.sprite = mark == Mark.Cross ? CrossSprite : NoughtSprite;
+            renderer.enabled = true;
+            DisableCollider();
+        }
+    }
+
+    private void DisableCollider()
+    {
+        var collider = GetComponent<BoxCollider2D>();
+        collider.enabled = false;
+    }
+
+    public void OnGameOver(IGame game, Line winnerLine, Mark mark)
+    {
+        DisableCollider();
+    }
+
+    public void OnNextPlayerTurn(IGame game, Mark mark)
+    {
+
+    }
 }
